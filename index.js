10475dfb142058bcd85e9f545c53c484e86749c2
Bo Eun Lee <bolee417@gmail.com>
2018-01-07 22:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,16 +26,7 @@ function makeInt (n) {
   return parseInt(n, 10);
 }
 
-function add (a, b) {
-  return a + b;
+function preserveDecimal (n) {
+  return parseFloat(n);
 }
 
-function add (a, b) {
-  return a + b;
-}
-
-function add (a, b) {
-  return a + b;
-}
-
-function subtract 
